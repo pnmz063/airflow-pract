835c4d817c01b5f6cd26467a3fd9ee8538b78d20
andrei_kartasov <anpkartashov@beeline.ru>
2023-01-17 02:16:27 +0200

    Initial commit

---
+++ python_dags/lect1/1_first_dag.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+dag = DAG(
+    "first_dag",
+    start_date=datetime(2023, 1, 15),
+    description="Привет, мир",
+    schedule_interval=None,
+    tags=["airflow_practice"]
+)
+
+
+def print_hello_world():
+    print("Hello, world! ps: with python")
+
+
+with_bash = BashOperator(
+    task_id="bash_task",
+    bash_command="echo Hello, world! ps: with bash",
+    dag=dag
+)
+
+with_python = PythonOperator(
+    task_id="python_task",
+    python_callable=print_hello_world,
+    dag=dag
+)
+
+with_python >> with_bash
---
+++ python_dags/lect1/2_first_task_flow_dag.py
@@ -0,0 +1,25 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+
+@dag(
+    start_date=datetime(2023, 1, 15),
+    description="Привет, мир и task flow api",
+    schedule_interval=None,
+    tags=["airflow_practice"]
+)
+def first_task_flow_api_dag():
+
+    @task
+    def print_hello_world():
+        print("Hello, world!")
+
+    @task
+    def print_something_else():
+        print("and something else")
+
+    print_hello_world()
+    print_something_else()
+
+
+first_task_flow_api_dag()
